0a886364b8f23c053eb3ad5f0b0122c04bd7ff2b
mwurdeman <mwurdeman@gmail.com>
2014-09-22 12:14:02 -0700

    Create new Project and added Recipe class

---
+++ Source/RecipeHelper/RecipeHelper.Domain/Recipe.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeHelper.Domain
+{
+    public class Recipe
+    {
+        public int RecipeID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Source { get; set; }
+        public string Servings { get; set; }
+        public string PrepTime { get; set; }
+        public string TotalRecipeTime { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
